ae638b0ce65437447ec978717f098074463c9a30
kiskisnotdead <akimin97@mail.ru>
2021-08-09 21:04:45 +0300

    Верстка

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button";
+import Maket from './Maket'
+
+function Main() {
+    return(
+        <div className="main">
+            <Maket/>
+            <Button/>
+        </div>
+    )
+}
+
+
+export default Main;
---
+++ src/components/Maket.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Maket() {
+    return( 
+        <div className="maket">
+            <p>gfdgd</p>
+            <input placeholder="*Введите текст"></input>
+        </div>
+    )
+}
+
+export default Maket;
